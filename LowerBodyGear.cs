349bfdb136fae72ef78e02b5a4dca431c618894d
RedSlimeKing <DevonALy@outlook.com>
2017-01-14 01:40:15 -0500

    Revert "Proper solution setup"
    This reverts commit 259ca84528bc452ba9262b02ad0af4c944d98d40.

---
+++ LowerBodyGear.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Our_Game
+{
+    class LowerBodyGear : Equipment
+    {
+        public LowerBodyGear(int level)
+        {
+            switch (level)
+            {
+                case 1:
+                    Name = "Coppper Pants";
+                    Type = "Lower Body Gear";
+                    IHP = 2;
+                    break;
+                case 2:
+                    Name = "Silver Pants";
+                    Type = "Lower Body Gear";
+                    IHP = 4;
+                    break;
+                case 3:
+                    Name = "Gold Pants";
+                    Type = "Lower Body Gear";
+                    IHP = 6;
+                    break;
+                case 4:
+                    Name = "Goat Legs";
+                    Type = "Lower Body Gear";
+                    IHP = 8;
+                    break;
+            }
+        }
+    }
+}
---
+++ Mob.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Our_Game
+{
+    class Mob
+    {
+        static int dmg;
+        string name;
+        static int currhp;
+        int Potion;
+        public static int def;
+
+        public Mob(string _name)
+        {
+            name = _name;
+            dmg = 5;
+            currHp = 50;
+            Potion = 5;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public static int currHp
+        {
+            get { return currhp; }
+            set { currhp = value; }
+        }
+
+        public static int MDamage
+        {
+            get { return dmg; }
+            set { dmg = value; }
+        }
+
+
+        public void Combat()
+        {
+            Random gen = new Random();
+            int n1 = gen.Next(1, 101);
+            int choice = gen.Next(1, 4);
+            if (n1 <= 15) // probability of 15%
+                choice = 4;
+            else if (n1 >= 60) // probability of 40%
+                choice = 1;
+            def = 0;
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Monster attacks");
+                    Attack(MDamage);
+                    break;
+                case 2:
+                    Console.WriteLine("Monster Defend");
+                    Defend();
+                    break;
+                case 4:
+                    Console.WriteLine("Monster uses potion");
+                    UsePotion();
+                    break;
+                case 3:
+                    Console.WriteLine("Monster farted");
+                    break;
+            }
+        }
+
+        public void Attack(int damage)
+        {
+            if (Player.def == 1)
+            {
+                //Player Blocks
+                Console.WriteLine("Blocked monster attack");
+            }
+            else
+            {
+                Player.ReceiveDamage(damage);
+            }
+        }
+
+        public void Defend()
+        {
+            def++;
+        }
+
+        public static void ReceiveDamage(int amount)
+        {
+            Mob.currHp -= amount;
+        }
+
+        public void UsePotion()
+        {
+            if (Potion > 0)
+            {
+                currHp += 20;
+                Potion--;
+            }
+            else
+            {
+                Potion = 0;
+            }
+        }
+    }
+}
